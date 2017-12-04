72374a39041cb1286e13d2f65e5a3970c759432c
myw219 <myw219@lehigh.edu>
2017-12-04 18:58:42 -0500

    tried reading the file

---
+++ FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+
+class FunctionalTextAnalysis {
+  /*
+   *val file = Source.fromFile("src/main/top40.sql").getLines
+  for(row <- 0 until file.{
+
+  }
+  */
+
+  val filename = "main/src/top40.sql"
+
+
+}
