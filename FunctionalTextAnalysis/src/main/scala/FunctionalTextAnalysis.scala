6f6eb29db0ce4bdac9561ba7a631d288834b9403
myw219 <myw219@lehigh.edu>
2017-12-05 01:16:27 -0500

    reads in the file

--- FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
+++ FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
@@ -1,14 +1,28 @@
+/**
+  * Matthew Wong
+  * myw219@lehigh.edu
+  * Assignment 5: Functional Text Analysis - takes in a file of the top 40 songs and checks for the probability
+  * of getting #1
+  *
+  *
+  */
+
+
+
 import scala.io.Source
 
 class FunctionalTextAnalysis {
-  /*
-   *val file = Source.fromFile("src/main/top40.sql").getLines
-  for(row <- 0 until file.{
 
+  def main(args: Array[String]): Unit = {
+
+    val file = Source.fromFile("src/main/top40.sql").getLines().toList
+    file.foreach(line => println(line))
+
+    /*
+    val filename = "main/src/top40.sql"
+    filename.foreach(println(_))
   }
   */
 
-  val filename = "main/src/top40.sql"
-
-
+  }
 }
