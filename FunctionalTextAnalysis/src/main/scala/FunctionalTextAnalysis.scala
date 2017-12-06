fa87c223cf46116394e13382e4c3b81e8327b79f
Matthew Wong <myw219@lehigh.edu>
2017-12-05 19:04:22 -0500

    small changes

--- FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
+++ FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
@@ -15,10 +15,22 @@ class FunctionalTextAnalysis {
 
   def main(args: Array[String]): Unit = {
 
-    val file = Source.fromFile("src/main/top40.sql").getLines().toList
-    file.foreach(line => println(line))
+    //reads in the top40 file and splits it into a tuple3
+    val file = Source.fromFile("/src/main/top40.sql").getLines.map(line => line.split(" ' "))
 
+
+    //val file = Source.fromFile("src/main/top40.sql").getLines.map(word => word.split(" ' ").toList).toList
+    //file.foreach(line => println(line))
+
+    //creates the tuple3 to store the song information
     /*
+    def createTuple3 (artist: List[String], song: List[String], num1: Int) = {
+
+    }
+    */
+
+
+    /* used to see if I can read in
     val filename = "main/src/top40.sql"
     filename.foreach(println(_))
   }
