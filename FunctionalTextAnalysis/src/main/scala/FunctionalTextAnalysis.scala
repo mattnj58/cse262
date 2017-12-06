467d2dfdebbab3557c95031e1eecdf3caf81890f
Matthew Wong <myw219@lehigh.edu>
2017-12-06 18:28:52 -0500

    fixed stuff

--- FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
+++ FunctionalTextAnalysis/src/main/scala/FunctionalTextAnalysis.scala
@@ -1,57 +1,59 @@
 /**
   * Matthew Wong
+  * CSE 262
   * myw219@lehigh.edu
   * Assignment 5: Functional Text Analysis - takes in a file of the top 40 songs and checks for the probability
   * of getting #1
   *
-  *
   */
-
-
 
 import scala.io.Source
 
-class FunctionalTextAnalysis {
+object FunctionalTextAnalysis extends App {
 
-  def main(args: Array[String]): Unit = {
+  //the non-trivial words in the song name
+  val nonTrivial = Array("LOVE", "ME", "IN", "CHIQUITITA")
 
-    //reads in the top40 file and splits it into a tuple3
-    val file = Source.fromFile("/src/main/top40.sql").getLines.map(line => line.split(" ' ")).toList
-    val numone = file(4).map(line=>line.split("\\s+")).toList
-    val song = (file(1), file(3).map(line => line.split("\\s+")), if(numone(1)==0) false else true)
+  //reads in the top40 file and splits it into a tuple3
+  val file = Source.fromFile("top40.txt").getLines.map(line => {
+    val ln = line.split("'")
+    val artist = ln(1)
+    val songName = ln(3).split("\\s+")
+    val numone = ln(4).charAt(2).toInt
+    (artist, songName, numone)
+  })
 
+  //variable that contains the Set (a list of attributes that doesn't repeat) of artists and songs
+  val lstArtist = file.foreach(record => record._1)
+  val lstSongs = file.foreach(record => record._2)
 
-    /* used to see if I can read in
-    val filename = "main/src/top40.sql"
-    filename.foreach(println(_))
+  //the count method that counts how many of something there is
+  //def counter()
+
+  //the multiplication function
+  def *(int1: Int, int2: Int): Int ={
+    val prod = int1 * int2
+    prod
   }
-  */
 
-    //the multiplication function
-    def *(int1: Int, int2: Int): Int ={
-      val prod = int1 * int2
-      prod
+  // the division function
+  //should not work if the denominator is 0
+  def /( numer: Int, denom: Int) = {
+    if(denom!=0) {
+      val divide = numer / denom
+      divide
+    } else {
+      System.exit(0)
     }
+  }
 
-    // the division function
-    //should not work if the denominator is 0
-    def /( numer: Int, denom: Int) = {
-      if(denom!=0) {
-        val divide = numer / denom
-        divide
-      } else {
-        System.exit(0)
-      }
-    }
+  //the addition method
+  def +(int1: Int, int2: Int) = {
+    val sum = int1 + int2
+  }
 
-    //the addition method
-    def +(int1: Int, int2: Int) = {
-      val sum = int1 + int2
-    }
-
-    //the subtraction method takes the second number from the first
-    def -(int1:Int, int2: Int) = {
-      val sub = int1 - int2
-    }
+  //the subtraction method takes the second number from the first
+  def -(int1:Int, int2: Int) = {
+    val sub = int1 - int2
   }
 }
